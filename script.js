8908ef586f67fb1a9fbc61c62d390a73ac378792
EricWendel <eawendel19@gmail.com>
2022-12-21 17:30:04 -0600

    Toggle resume

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function closeResume() {
   document.getElementById("closeButton").style.display = "none";
   document.getElementById("body").style.overflowY = "visible";
 }
+
+function toggleResume() {
+  if (document.getElementById("myResume").style.display != "block") {
+    openResume();
+  } else {
+    closeResume();
+  }
+}
